6d32bc881832f46482275df1c083d9cd5f37eb96
sashabuzin <n9159648714@gmail.com>
2021-12-08 20:51:28 +0300

    Lesson 1 is finished

--- app/src/main/java/com/buzinasgeekbrains/searchbymovies/MainActivity.kt
+++ app/src/main/java/com/buzinasgeekbrains/searchbymovies/MainActivity.kt
@@ -2,10 +2,29 @@ package com.buzinasgeekbrains.searchbymovies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        testFor()
+        val myPerson : Person = Person("Александр", 28)
+        val button : Button = findViewById(R.id.button)
+        val textView : TextView = findViewById(R.id.text_view)
+        val result: String = "Имя: " + myPerson.name + "\n" + "Возраст: " + myPerson.age + "лет."
+        button.setOnClickListener{textView.text = result}
+
+
+
+    }
+
+    fun testFor() {
+        for (i in 50 downTo 29 step 3) {
+            println(i)
+        }
     }
 }
